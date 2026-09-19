dde7ac8d78aa2aa4b6a1e6c2af83e258473784b3
prabuddhapradeep103-lang <prabuddhapradeep103@gmail.com>
2026-09-20 00:52:09 +0530

    feat: Define macro constants and global lookup arrays in header

--- hos.h
+++ hos.h
@@ -4,18 +4,18 @@
 #include <stdio.h>
 #include <string.h>
 
-
+// Macro Constants
 #define MAX_PATIENTS 100
 #define NUM_SPECIALTIES 4
 #define NUM_WARDS 4
 
-
+// Global Lookup Arrays
 extern const float BASE_FEES[NUM_SPECIALTIES];
 extern const int CONSULT_TIMES[NUM_SPECIALTIES];
 extern const float WARD_RATES[NUM_WARDS];
 extern const int WARD_CAPACITIES[NUM_WARDS];
 
-
+// Parallel Arrays for Patient Data
 extern char p_names[MAX_PATIENTS][50];
 extern int p_ages[MAX_PATIENTS];
 extern int p_urgency[MAX_PATIENTS];
@@ -26,21 +26,7 @@ extern float p_gross[MAX_PATIENTS];
 extern float p_discount[MAX_PATIENTS];
 extern float p_final[MAX_PATIENTS];
 
-
+// System State Tracking
 extern int patient_count;
 extern int spec_queue[NUM_SPECIALTIES];
 extern int bedOccupancy[NUM_WARDS][20];
-
-
-void initializeSystem(void);
-void displayMenu(void);
-void registerPatient(void);
-void displayBedMatrix(void);
-void sortByPriority(void);
-void displayPriorityList(void);
-void generateReports(void);
-void saveBedsToFile(void);
-void loadBedsFromFile(void);
-void appendPatientRecordToFile(int index);
-
-#endif
